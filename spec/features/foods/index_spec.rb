00828d0ad164e07a71ef44f09f286ee14f3fc263
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-21 09:27:37 -0600

    test: foods index search routes to foods

---
+++ spec/features/foods/index_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'Foods Search Index', type: :feature do
+  before(:each) do
+    visit root_path
+   save_and_open_page
+    fill_in :foods, with: "sweet potatoes"
+    click_on "Search"
+  end
+
+  describe 'results' do
+    it 'search routes to /foods index' do 
+      expect(current_path).to eq(foods_path)
+    end
+    xit 'displays number of items returned by search' do 
+      expect(page).to have_css(".result_number")
+    end
+#(sweet potatoes should find more than 30,000 results)
+    
+    it "lists ten foods that contain 'sweet potatoes'"
+
+    it 'lists attributes of those foods'
+    #The food's GTIN/UPC code
+    # The food's description
+    # The food's Brand Owner
+    # The food's ingredients
+  end
+end
